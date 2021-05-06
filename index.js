09b5c0c7b6aa37df6903bf070b95e5ae14692f5c
kennytran275 <kennyt275@gmail.com>
2021-05-06 19:36:19 -0400

    wip

---
+++ index.js
@@ -0,0 +1,17 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const server = express();
+const PORT = process.env.PORT || 5000;
+
+server.use(express.json());
+
+server.use("/api/*", (req, res) => {
+    res.json({
+        cohort:"Web 41",
+    });
+});
+
+server.listen(PORT, () => {
+    console.log("listening on ", PORT);
+})
+
